84a126d412fc5a98d34b3d646f0b793a41143e42
Sina Sakhaei <cnasakhaei@gmail.com>
2023-09-05 13:02:31 +0330

    final file

---
+++ weather/views.py
@@ -0,0 +1,78 @@
+from django.shortcuts import render
+from django.shortcuts import render
+import requests
+from datetime import datetime
+
+def get_weather_info(city_name):
+    api_key = "7a275e09915223beec68641b735bfdb9"
+    get_geo_url = "http://api.openweathermap.org/geo/1.0/direct"
+
+    params = {
+        "q": city_name,
+        "appid": api_key
+    }
+
+    response = requests.get(url=get_geo_url, params=params)
+    response_json = response.json()
+    
+    if response_json:
+        lat = response_json[0]["lat"]
+        lon = response_json[0]["lon"]
+        
+        weather_url = "http://api.openweathermap.org/data/2.5/weather"
+        params = {
+            "lat": lat,
+            "lon": lon,
+            "appid": api_key
+        }
+
+        response = requests.get(weather_url, params=params)
+        weather_info = response.json()
+
+        if weather_info['cod'] == 200:
+            temp = int(weather_info['main']['temp'] - 273)
+            feels_like_temp = int(weather_info['main']['feels_like'] - 273)
+            pressure = weather_info['main']['pressure']
+            humidity = weather_info['main']['humidity']
+            wind_speed = weather_info['wind']['speed'] * 3.6
+            sunrise = weather_info['sys']['sunrise']
+            sunset = weather_info['sys']['sunset']
+            timezone = weather_info['timezone']
+            cloudy = weather_info['clouds']['all']
+            description = weather_info['weather'][0]['description']
+
+            sunrise_time = time_format_for_location(sunrise + timezone)
+            sunset_time = time_format_for_location(sunset + timezone)
+            return {
+                "temp" : temp,
+                "feels" : feels_like_temp,
+                "pressure" : pressure,
+                "humidity" : humidity,
+                "wind_speed" : wind_speed,
+                "sunrise" : sunrise,
+                "sunset" : sunset,
+                "timezone" : timezone,
+                "cloudy" : cloudy,
+                "description" : description,
+                "sunrise_time" : sunrise_time,
+                "sunset_time" : sunset_time,
+
+            }
+            return f"Weather of: {city_name}\nTemperature (Celsius): {temp}°\nFeels like in (Celsius): {feels_like_temp}°\nPressure: {pressure} hPa\nHumidity: {humidity}%\nSunrise at {sunrise_time} and Sunset at {sunset_time}\nCloud: {cloudy}%\nInfo: {description}"
+        else:
+            return f"Weather for '{city_name}' not found! Kindly enter a valid City Name!"
+    else:
+        return f"City '{city_name}' not found! Kindly enter a valid City Name!"
+
+def time_format_for_location(utc_with_tz):
+    local_time = datetime.utcfromtimestamp(utc_with_tz)
+    return local_time.time()
+
+def show_weather(request):
+    city_name = request.GET.get('city_name', False).capitalize()
+    if city_name == False:
+        return render(request, 'weather/weather.html')
+    weather_info = get_weather_info(city_name)
+    return render(request, 'weather/weather.html', {'weather_info': weather_info, 'city_name':city_name})
+
+
